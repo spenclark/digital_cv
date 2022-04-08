12daf27779b40729d97d2825c7ae4409c6abb937
Spencer <spenclark@hotmail.com>
2022-04-08 14:04:13 -0700

    content init

---
+++ app/src/assets/Content/WrittenContent.js
@@ -0,0 +1,14 @@
+// Defualt data schema
+
+// {
+//     title: "",
+//     id: "", used to gnerate unique url
+//     docutmentTitle: "", set to document title for seo
+//     description: "", set to document description,
+//     paragraphs: [] // each item in arr should be treated like a paragraph
+// }
+
+
+const writtenContent = {
+    
+}

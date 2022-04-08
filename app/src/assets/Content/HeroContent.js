4c978bc72681d9950272e541e7a5e408a74e5846
Spencer <spenclark@hotmail.com>
2022-04-08 14:18:21 -0700

    test data for written content

---
+++ app/src/assets/Content/HeroContent.js
@@ -0,0 +1,7 @@
+export const heroContent = {
+  title: "Spencer Clark",
+  jobTitle: "Full-Stack Engineer",
+  aboutMe:
+    "My broad work interest is helping companies crush businesses problems through design and engineering. I think engineers can take deliverables from start to finish, and hiring practices broadly are not recognizing talent. I write about things that help other people like me make better engineering and talent decisions. I have some side projects primarily in the recruiting and HR industry, which has taught me a lot about time management, business & marketing and making real-world products.",
+  employmentStatus: "Open to full-time and contract roles.",
+};

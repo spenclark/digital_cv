4364ae4d4693f5673b0c2128830fbca6b33fb961
Spencer <spenclark@hotmail.com>
2022-04-08 15:21:00 -0700

    content

---
+++ app/src/assets/Content/NavContent.js
@@ -0,0 +1,31 @@
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { Link } from "@mui/material";
+
+export const navContent = {
+  timezone: "UTC−08:00",
+  location: "Canada",
+  repoLink: "https://github.com/spenclark/digital_cv",
+  buttons: [
+    {
+      icon: EmailIcon,
+      action: "",
+    },
+    {
+      icon: GitHubIcon,
+      action: "",
+    },
+    {
+      icon: TwitterIcon,
+      action: "",
+    },
+    {
+      icon: LinkedInIcon,
+      action: "",
+    },
+  ],
+};
+
+email, twitter, github, linkedin;
